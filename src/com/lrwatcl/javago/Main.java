4b90a2b551c4d22cd7597ac5678c9757f22bcbab
linrongwu <973218670@qq.com>
2021-04-12 20:17:59 +0800

    Initial commit

---
+++ src/com/lrwatcl/javago/Main.java
@@ -0,0 +1,13 @@
+package com.lrwatcl.javago;
+
+/**
+ *
+ * @author String
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
---
+++ src/com/lrwatcl/javago/charstring/code/CharString.java
@@ -0,0 +1,24 @@
+package com.lrwatcl.javago.charstring.code;
+
+/**
+ * TODO
+ * 字符
+ * @author String
+ * @data 2021/4/12 19:32
+ */
+public class CharString {
+
+    public void charFunction(){
+        char c = 'a';
+        Character character = 'a';
+
+    }
+
+    public void stringFunction(){
+        String string = "lrw a tcl";
+        StringBuilder stringBuilder = new StringBuilder("lrw a tcl");
+        StringBuffer stringBuffer = new StringBuffer("lrw a tcl");
+
+    }
+
+}
